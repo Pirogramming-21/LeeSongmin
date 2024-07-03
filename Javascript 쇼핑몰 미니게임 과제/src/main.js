d3a03ceafd2645e41a72b48a1f9e813b845786cb
songmin lee <violet2111@naver.com>
2024-07-04 02:37:00 +0900

    JavaScript-handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -27,11 +27,55 @@ function createHTMLString(item) {
     // ``: ''랑 다른거임. ~ 키보드에 같이
 }
 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return;
+    }
+
+    displayItems(items.filter(item => item[key] === value));
+    // 배열에서 특별한 item만 추출해서 새로운 단위의 배열 만들 때: filter()
+    // object는 배열처럼 key를 이용해서 데이터 접근 가능
+}
+
+function setEventListeners(items) { // 이벤트 위임. 한 곳에서 조작 가능하도록
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems() //item을 동적으로 받아옴
     .then(items => { //promise를 리턴. promise가 성공적으로 되면 items 받아오도록 함.
         //console.log(items); // 확인용
         displayItems(items); //HTML에 items를 보여줌
-        //setEventListeners(items); //버튼을 클릭했을 때 적절히 필터링
+        setEventListeners(items); //버튼을 클릭했을 때 적절히 필터링
     })
     .catch(console.log)
+
+/* 
+//위처럼 하면 계속 업데이트 해야해서 번거로움
+//Handle button click
+function onButtonClick(event, items) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+    if(key == null || value == null) {
+    return;
+    }
+    updateItems(items, key, value);
+}
+
+// Make the items matching {key: value} invisible.
+function updateItems(items, key, value) {
+    if(item.dataset[key] === value) {
+    item.classList.remove('invisible');
+    } else {
+        item.classList.add('invisible'); 
+    }
+}
+*/
